3ffc24981cb8f77cc200ae16fd7023c0a0386573
Hobbit <Hobbit@localhost>
2020-12-02 15:09:55 -0700

    Day1 and Day2

---
+++ Day1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Part1()
+        {
+            string inputPath = "input.txt";
+
+            List<int> lines = new List<string>(File.ReadAllLines(inputPath)).Select(x => int.Parse(x)).ToList();
+
+            foreach (var num1 in lines)
+            {
+                foreach(var num2 in lines)
+                {
+                    if(num1 != num2)
+                    {
+                        if(num1 + num2 == 2020)
+                        {
+                            Console.WriteLine("Part 1: " + num1 * num2);
+                            return;
+                        }
+                    }
+                }
+                
+            }
+
+            
+        }
+
+        static void Part2()
+        {
+            string inputPath = "input.txt";
+
+            List<int> lines = new List<string>(File.ReadAllLines(inputPath)).Select(x => int.Parse(x)).ToList();
+
+            foreach (var num1 in lines)
+            {
+                foreach (var num2 in lines)
+                {
+                    foreach (var num3 in lines)
+                    {
+                        if (num1 != num2 && num2 != num3 && num1 != num3)
+                        {
+                            if (num1 + num2 + num3 == 2020)
+                            {
+                                Console.WriteLine("Part 2: " + num1 * num2 * num3);
+                                return;
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Part1();
+            Part2();
+
+        }
+    }
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Part1()
+        {
+            string inputPath = "input.txt";
+
+            List<string> lines = new List<string>(File.ReadAllLines(inputPath));
+
+            int validPw = 0;
+            foreach (var line in lines)
+            {
+                //Extract password policy and password
+                string policy = line.Split(':')[0].Trim();
+                char[] pw = line.Split(':')[1].Trim().ToCharArray();
+
+                //Parse password policy
+                string numbers = policy.Split(' ')[0];
+                char letter = policy.Split(' ')[1].ToCharArray()[0];
+
+                int minLetters = Int32.Parse(numbers.Split('-')[0]);
+                int maxLetters = Int32.Parse(numbers.Split('-')[1]);
+
+                int numChars = pw.Count(c => c == letter);
+
+                if (numChars >= minLetters && numChars <= maxLetters)
+                {
+                    validPw++;
+                }
+            }
+
+            Console.WriteLine("Part 1: " + validPw.ToString());
+        }
+
+        static void Part2()
+        {
+            string inputPath = "input.txt";
+
+            List<string> lines = new List<string>(File.ReadAllLines(inputPath));
+
+            int validPw = 0;
+            foreach (var line in lines)
+            {
+                //Extract password policy and password
+                string policy = line.Split(':')[0].Trim();
+                char[] pw = line.Split(':')[1].Trim().ToCharArray();
+
+                //Parse password policy
+                string numbers = policy.Split(' ')[0];
+                char letter = policy.Split(' ')[1].ToCharArray()[0];
+
+                int firstLetter = Int32.Parse(numbers.Split('-')[0]);
+                int secondLetter = Int32.Parse(numbers.Split('-')[1]);
+
+                if(pw[firstLetter-1] == letter && pw[secondLetter-1] != letter ||
+                   pw[firstLetter-1] != letter && pw[secondLetter-1] == letter)
+                {
+                    validPw++;
+                }
+            }
+
+            Console.WriteLine("Part 2: " + validPw.ToString());
+        }
+
+        static void Main(string[] args)
+        {
+            Part1();
+            Part2();
+            
+        }
+    }
+}
